cad68adb1b0f97f93a43f1888d06543a46d49e66
Keith Johnson <kjcjohnson@ymail.com>
2023-08-16 21:18:14 -0500

    Types and classes for indexed identifier support

--- src/smt/theory-dispatch.lisp
+++ src/smt/theory-dispatch.lisp
@@ -6,29 +6,120 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defvar *builtin-smt-functions* (make-hash-table :test 'equal)))
 
+;;;
+;;; Types for names and indexes
+;;;
+(deftype smt-index-type (&key (concrete t))
+  `(or string integer ,@(unless concrete '(symbol))))
+
+(defun %smt-abstract-name-type-p (rest)
+  "Checks that REST satisfies the constraints to be an index"
+  (every #'(lambda (x) (typep x '(smt-index-type :concrete nil))) rest))
+
+(defun %smt-concrete-name-type-p (rest)
+  "Checks that REST satisfies the contraints to be a concrete index"
+  (every #'(lambda (x) (typep x '(smt-index-type :concrete t))) rest))
+
+(deftype smt-simple-name-type ()
+  'string)
+
+(deftype smt-indexed-name-type (&key (concrete t))
+  `(cons string (satisfies ,(if concrete
+                                '%smt-concrete-name-type-p
+                                '%smt-abstract-name-type-p))))
+
+(deftype smt-name-type (&key (concrete t))
+  `(or smt-simple-name-type (smt-indexed-name-type :concrete ,concrete)))
+
+;;;
+;;; Classes for theory declarations
+;;;
+(defclass theory-entry ()
+  ((smt-name :reader smt-name
+             :initarg :smt-name
+             :type smt-name-type
+             :documentation "The SMT name of this entry")
+   (fn-name :reader fn-name
+            :initarg :fn-name
+            :type symbol
+            :documentation "The name of the function underlying this entry")
+   (fn-impl :reader fn-impl
+            :initarg :fn-impl
+            :type function
+            :documentation "The function implementation for this entry"))
+  (:documentation "A theory function entry"))
+
+(defclass theory-family-entry (theory-entry)
+  ((param-indexes :reader param-indexes
+                  :initarg :param-indexes
+                  :type list
+                  :documentation "The index of symbols in the index that are args"))
+  (:documentation "A family of theory functions, i.e., indexed identifiers"))
+
+(defclass theory-fn-entry (theory-entry)
+  ()
+  (:documentation "A concrete theory function entry"))
+
+(defun %indexed-base-name (smt-name &key concrete)
+  (cond
+    ((atom smt-name)
+     (format nil "~a" smt-name))
+    ((= 1 (length smt-name))
+     (format nil "~a" (first smt-name)))
+    (concrete
+     (format nil "~a" smt-name))
+    (t
+     (format nil "~a;~a" (first smt-name) (length (rest smt-name))))))
+
+(defun %parse-indexed-name (smt-name)
+  "Parses data out of SMT-NAME"
+  (setf smt-name (a:ensure-list smt-name))
+  (let ((concrete (not (some #'symbolp (rest smt-name))))
+        (param-ixs nil)
+        (param-syms nil))
+    (loop for ix from 0
+          for x in (rest smt-name)
+          when (symbolp x)
+            collect ix into indexes
+            and collect x into symbols
+          finally (setf param-ixs indexes
+                        param-syms symbols))
+    (let ((base-name (%indexed-base-name smt-name :concrete concrete)))
+      (values base-name concrete param-ixs param-syms))))
+
 (defmacro defsmtfun (smt-name theory lambda-list &body body)
   "Defines a built-in SMT theory function named SMT-NAME. This function will be
 associated with a theory THEORY (denoted by a keyword) with given LAMBDA-LIST), and an
 implementation given by BODY."
-  (check-type smt-name (or string list))
-  (when (listp smt-name)
-    (assert (not (zerop (length smt-name))))
-    (check-type (first smt-name) string)
-    (map nil #'(lambda (x) (check-type x (or string integer symbol))) smt-name))
+  (check-type smt-name (smt-name-type :concrete nil))
   (check-type theory keyword)
-  (let ((fn-name (intern (format nil "SMT[~a]~a" theory smt-name))))
-    `(eval-when (:compile-toplevel :load-toplevel :execute)
-       (defun ,fn-name ,lambda-list
-         ,@body)
-       (setf (gethash ',(a:ensure-list smt-name) *builtin-smt-functions*)
-             (cons ',fn-name #',fn-name)))))
+  (setf smt-name (a:ensure-list smt-name))
+  (multiple-value-bind (base-name concrete param-ixs param-syms)
+      (%parse-indexed-name smt-name)
+    (let ((fn-name (intern (format nil "SMT[~a]~a~@[[~{~a~^,~}]~]"
+                                   theory smt-name param-ixs))))
+      `(eval-when (:compile-toplevel :load-toplevel :execute)
+         (defun ,fn-name ,(append param-syms lambda-list)
+           ,@body)
+         (setf (gethash ',base-name *builtin-smt-functions*)
+               ,(if concrete
+                    `(make-instance 'theory-fn-entry
+                                    :smt-name ',smt-name
+                                    :fn-name ',fn-name
+                                    :fn-impl #',fn-name)
+                    `(make-instance 'theory-family-entry
+                                    :smt-name ',smt-name
+                                    :fn-name ',fn-name
+                                    :fn-impl #',fn-name
+                                    :param-indexes ',param-ixs)))))))
 
 (defun lookup-theory-function (name)
   "Looks up an SMT theory function"
-  (setf name (a:ensure-list name))
+  (declare (type smt-name-type name))
+  (setf name (%indexed-base-name name))
   (let ((looked-up (gethash name *builtin-smt-functions*)))
     (unless (null looked-up)
-      (return-from lookup-theory-function (cdr looked-up))))
+      (return-from lookup-theory-function (fn-impl looked-up))))
 
   ;; Not a theory function
   nil)
@@ -36,15 +127,16 @@ implementation given by BODY."
 (defun map-built-in-definitions (operation)
   "Iterates over built-in function definitions. OPERATION accepts two args"
   (maphash #'(lambda (key value)
-               (funcall operation key (cdr value)))
+               (funcall operation key (fn-impl value)))
            *builtin-smt-functions*))
 
 (defun lookup-theory-function-symbol (name)
   "Looks up a function name for a theory function"
-  (let ((looked-up (gethash (a:ensure-list name) *builtin-smt-functions*)))
+  (declare (type smt-name-type name))
+  (let ((looked-up (gethash (%indexed-base-name name) *builtin-smt-functions*)))
     (if (null looked-up)
         nil
-        (car looked-up))))
+        (fn-name looked-up))))
 
 (defun do-call-smt (fnname &rest args)
   "Calls an SMT function with arguments ARGS. Looks up FNNAME at runtime - prefer using
--- t/smt/theories/main.lisp
+++ t/smt/theories/main.lisp
@@ -5,3 +5,47 @@
 
 (5am:def-suite :smt-theories :in :smt-tests)
 (5am:in-suite :smt-theories)
+
+;;;
+;;; Type tests
+;;;
+(5am:test smt-index-type
+  (5am:is-true (typep "test" 'smt::smt-index-type))
+  (5am:is-true (typep 1 'smt::smt-index-type))
+  (5am:is-false (typep 'x 'smt::smt-index-type))
+  (5am:is-true (typep 'x '(smt::smt-index-type :concrete nil)))
+  (5am:is-false (typep 3.3 'smt::smt-index-type)))
+
+(5am:test smt-simple-name-type
+  (5am:is-true (typep "test" 'smt::smt-simple-name-type))
+  (5am:is-false (typep '("test" 1 2) 'smt::smt-simple-name-type))
+  (5am:is-false (typep 5 'smt::smt-simple-name-type)))
+
+(5am:test smt-indexed-name-type
+  (5am:is-false (typep "test" 'smt::smt-indexed-name-type))
+  (5am:is-true (typep '("test" 1 2) 'smt::smt-indexed-name-type))
+  (5am:is-false (typep '("test" 1 2 q) 'smt::smt-indexed-name-type))
+  (5am:is-true (typep '("test" 1 2 q) '(smt::smt-indexed-name-type :concrete nil)))
+  (5am:is-false (typep '(x 1 2) 'smt::smt-indexed-name-type)))
+
+(5am:test smt-name-type
+  (5am:is-true (typep "test" 'smt::smt-name-type))
+  (5am:is-true (typep '("test" 1 "2") 'smt::smt-name-type))
+  (5am:is-false (typep '("test" 1 2 q) 'smt::smt-name-type))
+  (5am:is-true (typep '("test" 1 2 q) '(smt::smt-name-type :concrete nil)))
+  (5am:is-false (typep 'x 'smt::smt-name-type)))
+
+;;;
+;;; Dispatch tests
+;;;
+(5am:test indexed-name-parsing
+  (5am:is (equal '("test" t nil nil)
+                 (multiple-value-list (smt::%parse-indexed-name "test"))))
+  (5am:is (equal '("test" t nil nil)
+                 (multiple-value-list (smt::%parse-indexed-name '("test")))))
+  (5am:is (equal '("(test 1 x)" t nil nil)
+                 (multiple-value-list (smt::%parse-indexed-name '("test" 1 "x")))))
+  (5am:is (equal '("test;2" nil (0 1) (i j))
+                 (multiple-value-list (smt::%parse-indexed-name '("test" i j)))))
+  (5am:is (equal '("test;3" nil (2) (q))
+                 (multiple-value-list (smt::%parse-indexed-name '("test" "x" 5 q))))))
